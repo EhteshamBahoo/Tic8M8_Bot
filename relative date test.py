b232dabfc56f76462669f56f2f63fa36306462dd
Ehtesham Bahoo <57722688+EhteshamBahoo@users.noreply.github.com>
2023-10-03 16:31:53 +0400

    working with relative date spacy

--- relative date test.py
+++ relative date test.py
@@ -6,28 +6,38 @@ class RelativeDate():
         return "return_relative_date"
 
 
+import spacy
+from datetime import datetime, timedelta
+
+# Load the spaCy English language model
+nlp = spacy.load("en_core_web_sm")
 
 def get_relative_date():
     while True:
         try:
-            user_input = input("Enter a relative date (next month/next week/tomorrow/today): ").strip().lower()
+            user_input = input("Enter a date expression (e.g., 'next month', 'next week', 'tomorrow', 'today'): ").strip().lower()
 
             # Get the current date
             current_date = datetime.now()
 
-            if user_input == 'next month':
-                result_date = current_date + timedelta(days=30)
-            elif user_input == 'next week':
-                result_date = current_date + timedelta(weeks=1)
-            elif user_input == 'tomorrow':
-                result_date = current_date + timedelta(days=1)
-            elif user_input == 'today':
-                result_date = current_date
-            else:
-                print("Invalid input. Please enter 'next month', 'next week', 'tomorrow', or 'today'.")
-                continue
+            # Process the user input using spaCy
+            doc = nlp(user_input)
 
-            return result_date.strftime("%Y-%m-%d")
+            # Interpret the date expression
+            for token in doc:
+                if token.text == "next" and token.head.text in ["month", "week"]:
+                    if token.head.text == "month":
+                        result_date = current_date + timedelta(days=30)
+                    elif token.head.text == "week":
+                        result_date = current_date + timedelta(weeks=1)
+                    return result_date.strftime("%Y-%m-%d")
+                elif token.text == "tomorrow":
+                    result_date = current_date + timedelta(days=1)
+                    return result_date.strftime("%Y-%m-%d")
+                elif token.text == "today":
+                    return current_date.strftime("%Y-%m-%d")
+
+            print("Invalid input. Please try again.")
         except ValueError:
             print("Invalid input format. Please try again.")
 
