2a25185bc63338ac4fb994193f98bff189850ad1
Ehtesham Bahoo <57722688+EhteshamBahoo@users.noreply.github.com>
2023-10-04 18:19:26 +0400

    commiy

--- actions/actions.py
+++ actions/actions.py
@@ -80,30 +80,65 @@ class ActionEventSearch(FormValidationAction):
         params = {
             "city": event_city,
             "category": event_category,
-            "maxprice": max_price,
         }
+
+        if max_price is not None and max_price.lower() == "free":
+            params["isfree"] = True
+        else:
+            params["maxprice"] = max_price
 
         event_api = EventAPI()
         events = event_api.get_events(params)
 
         if events:
-            event_list = []
+            coursel_elements = []
 
             for event in events:
                 event_name = event.get("event_name", "N/A")
                 event_location = event.get("street", "N/A")
+                image_name = event.get("image_name", "N/A")
+                externallink = event.get("externallink", "N/A")
 
-                event_info = f"Event: {event_name} at address: {event_location}"
-                event_list.append(event_info)
+                coursel_element = {
+                    "title": event_name,
+                    "subtitle": event_location,
+                    "image_url": f"https://tic8m8.com/uploads/events/{image_name}",
+                    "buttons": [
+                        {
+                            "title": "Contact Information",
+                            "payload": f"Contact Information for {event_name}",
+                            "type": "postback"
+                        },
+                        {
+                            "title": "More Info",
+                            "payload": f"More Information of {event_name}",
+                            "type": "postback"
+                        },
+                        {
+                            "title": "More Details",
+                            "url": externallink,
+                            "type": "web_url"
+                        }
+                    ]
+                }
+                coursel_elements.append(coursel_element)
 
-            response_message = "Here is the list of events:\n\n" + "\n".join(event_list)
-            dispatcher.utter_message(response_message)
+            coursel_message = {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": coursel_elements
+                }
+            }
+            dispatcher.utter_message(attachment=coursel_message)
         else:
             dispatcher.utter_message(
                 "There aren't any events based on your criteria. Why don't you check our events page? We have a whole catalog of events there! 😀"
             )
 
         return []
+
+
 
 
 
@@ -152,7 +187,7 @@ class ActionListAllEvents(Action):
 
 
 """ end """
- 
+    
 """ Give ticket type information"""
 class ActionGetTicketTypes(Action):
     def name(self) -> Text:
