97cd81834dc5797cdea0bdadab48b0b10918b455
Ehtesham Bahoo <57722688+EhteshamBahoo@users.noreply.github.com>
2023-10-17 10:53:05 +0400

    push

--- actions/actions.py
+++ actions/actions.py
@@ -6,7 +6,6 @@ from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 from dateutil import parser
-import spacy
 from datetime import datetime, timedelta
 import re
 import requests
